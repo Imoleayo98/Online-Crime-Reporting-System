1bf5588655d84688fdc0312685acfd483f0e4cf3
Vidit Shah <svidit651@gmail.com>
2023-05-03 15:57:27 +0530

    add fir csr img

--- ocrs/views.py
+++ ocrs/views.py
@@ -5,7 +5,9 @@ from django.contrib.auth import authenticate, login, logout
 from complaints.models import *
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from datetime import datetime
 from accounts.models import *
+import pytz
 
 def landing_page(request):
     return render(request,"landing_page.html")
@@ -213,32 +215,40 @@ def register_fir_csr(request):
     if request.method == 'POST':
         complaint_type = request.POST.get('complaint_type')        
         complaint_id = request.POST.get('complaint_id')
-        print(f"complaint_id: {complaint_id}")   
+        print(f"complaint_id: {complaint_type}")   
         complainant_name = request.POST.get('name')
         complainant_gender = request.POST.get('gender')
         complainant_contact_no = request.POST.get('contact_no')
         complainant_email = request.POST.get('email')
-        complainant_dob = request.POST.get('dob')
+        complainant_dob = complaint_master.objects.get(complaint_id=complaint_id).complainant_dob
         complainant_address = request.POST.get('address')
-        complainant_state_name = request.POST.get('complainant_state_name')
-        complainant_state_id = state_master.objects.get(state_name=complainant_state_name).state_id
-        complainant_district_name = request.POST.get('complainant_district_name')
-        complainant_district_id = district_master.objects.get(district_name=complainant_district_name).district_id
+        get_complainant_state_name = request.POST.get('complainant_state_name')
+        complainant_state_name = state_master.objects.get(state_name=get_complainant_state_name)
+        complainant_state_id = state_master.objects.get(state_name=get_complainant_state_name).state_id
+        get_complainant_district_name = request.POST.get('complainant_district_name')
+        complainant_district_name = district_master.objects.get(district_name=get_complainant_district_name)
+        complainant_district_id = district_master.objects.get(district_name=get_complainant_district_name).district_id
         complainant_pin_code = request.POST.get('pincode')
-        state_name = request.POST.get('state_name')
-        state_id = state_master.objects.get(state_name=state_name).state_id
-        district_name = request.POST.get('district_name')
-        district_id = district_master.objects.get(district_name=district_name).district_id
-        station_name = request.POST.get('station_name')
-        station_id = police_station_master.objects.get(station_name=station_name).station_id
-        crime_category = request.POST.get('crime_category')
+        get_state_name = request.POST.get('state_name')
+        state_name = state_master.objects.get(state_name=get_state_name)
+        state_id = state_master.objects.get(state_name=get_state_name).state_id
+        get_district_name = request.POST.get('district_name')
+        district_name = district_master.objects.get(district_name=get_district_name)
+        district_id = district_master.objects.get(district_name=get_district_name).district_id
+        get_station_name = request.POST.get('station_name')
+        station_name = police_station_master.objects.get(station_name=get_station_name)
+        station_id = police_station_master.objects.get(station_name=get_station_name).station_id
+        crime_category = crime_category_master.objects.get(crime_category_name=request.POST.get('crime_category'))
         other_crime_category = request.POST.get('other_crime_category')
         subject = request.POST.get('subject')
         detailed_description = request.POST.get('detailed_description')
         delay_reason = request.POST.get('delay_in_complaining')
-        datetime_of_occurence = request.POST.get('date_time_of_occurence')
+
+        datetime_of_occurence = complaint_master.objects.get(complaint_id=complaint_id).datetime_of_occurence
+
+
         place_of_occurence = request.POST.get('place_of_occurence')
-        evidence_image = request.POST.get('evidence_image')
+        evidence_image = complaint_master.objects.get(complaint_id=complaint_id).evidence_image
         info_by_station_incharge = request.POST.get('info_by_incharge')
         if(complaint_type == 'fir'):
             fir_no = request.POST.get('fir_no')
@@ -295,11 +305,16 @@ def register_fir_csr(request):
             created_at = timezone.now(),
             updated_at = timezone.now()
             )
+            complaint = complaint_master.objects.get(complaint_id=complaint_id)
+            complaint.status = "FIR is Filed"
+            complaint.save()
+            return redirect('police_incharge_view_complaint')
 
 
 
         elif(complaint_type == 'csr'):
-            csr_no = request.POST.get('csr_no'),
+            csr_no = request.POST.get('csr_no')
+            print(csr_no)
             csr = csr_master.objects.create(
                 csr_no=csr_no,
                 complainant_name = complainant_name,
@@ -332,6 +347,11 @@ def register_fir_csr(request):
                 created_at = timezone.now(),
                 updated_at = timezone.now()
             )
+            print(csr)
+            complaint = complaint_master.objects.get(complaint_id=complaint_id)
+            complaint.status = "CSR is Filed"
+            complaint.save()
+            return redirect('police_incharge_view_complaint')
         
         else:
             complaint = complaint_master.objects.get(complaint_id=complaint_id)
