ea5b60831d14a8de1b525d5ab55f0d101efb31e0
Ronak Pandey <ronakpandey8304@gmail.com>
2023-05-05 19:22:15 +0530

    admin panel label

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,3 +4,6 @@ from .models import *
 admin.site.register(CustomUser)
 admin.site.register(police_incharge)
 admin.site.register(police_officer)
+admin.site.site_header = "O.C.R.S Admin"
+admin.site.site_title = "O.C.R.C Admin Portal"
+admin.site.index_title = "Welcome to Online Crime Reporting System Admin Panel"
